491e785c4ee7e2c34221a8574bdb6d9e7b540e97
M. Chowdhury <mintychocolate101@gmail.com>
2020-08-03 15:06:42 -0700

    Hadoop pipeline complete

--- extract_detect/Hadoop/SonarImage.java
+++ extract_detect/Hadoop/SonarImage.java
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.OutputStream;
 import java.io.ByteArrayOutputStream;
-//import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
@@ -35,30 +34,18 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-//import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
-//import org.w3c.dom.Text;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.JobContext;
 import org.apache.hadoop.mapreduce.RecordReader;
 import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.hadoop.mapreduce.lib.input.*;
-//import org.apache.hadoop.mapred.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 
 /**
- * Hadoop with Multiple Outputs, both image and metadata version
- * 
- * Linux commands: $ javac -cp /usr/lib/hadoop/*:/usr/lib/hadoop/client-0.20/*
- * -d Host_Exchange/HadoopSonar/SonarImage
- * Host_Exchange/HadoopSonar/SonarImage/SonarImage.java
- * Host_Exchange/HadoopSonar/SonarImage/Extract.java
- * Host_Exchange/HadoopSonar/SonarImage/Util.java $ jar -cvf SonarImage.jar -C
- * Host_Exchange/HadoopSonar/SonarImage . $ hadoop jar SonarImage.jar SonarImage
- * /user/cloudera/Host_Exchange/remusfiles/florida
- * /user/cloudera/Host_Exchange/HadoopSonar/SonarImage/output ^ change name of
- * output folder in third line accordingly
+ * Hadoop with Multiple Outputs: mine-tagged image and metadata
+ * @author Kaylin Li, Pyojeong Kim, Mitali Chowdhury, Seth Knoop, Nancy Daoud
  */
 
 public class SonarImage {
@@ -67,29 +54,33 @@ public class SonarImage {
 	// also have the file be placed in the output folder
 	public static String basePath = "/home/cloudera/Documents/shared/MineProcess/output/";
 
-	// file count is attached to name of png to see if multiple pngs are being
-	// printed
+	// file count is attached to name of png
 	public static int count = 0;
 
 	// these paths don't seem to work. It seems like after the main function, the
 	// variables here are reset
 	public static Path inPath;
 	public static Path outPath;
-	// conf seems to be fine but that's mainly because we don't need its value from
-	// the main function
+	
 	public static Configuration conf;
-
-	public static byte[] bytes;
-	public static byte[][] all_bytes = null;
-	public static byte[][] image = null;
-	public static int bytes_length;
-	public static String currentkey;
 	private static Job job;
 
+	// variables for storage of image data
+	public static byte[] bytes;
+	public static byte[][] right_bytes = null;
+	public static byte[][] left_bytes = null;
+	public static byte[][] image = null;
+	public static int bytes_length;
+	public static String lastkey;
+	public static String currentkey;
+
 	/**
-	 * Mapper with: input key = file name (Text.class) input value = MSTIFF binary
-	 * data (BytesWritable.class) output key = file name (Text.class) output value =
-	 * image in byte form (BytesWritable.class)
+	 * Mapper with: input key = file name (Text.class) 
+	 * input value = MSTIFF binary data (BytesWritable.class) 
+	 * 
+	 * output key = file name (Text.class) 
+	 * output value = image with mine tagged in byte form (BytesWritable.class)
+	 * output also includes CSV files for metadata and image information
 	 */
 	public static class Sonar_Mapper extends Mapper<Text, BytesWritable, Text, Text> {
 
@@ -97,13 +88,12 @@ public class SonarImage {
 
 		public void setup(Context context) throws IOException, InterruptedException {
 			mos = new MultipleOutputs(context);
-
 		}
 
 		@Override
 		public void map(Text key, BytesWritable value, Context context) throws IOException, InterruptedException {
 
-			// process the files
+			// extract data from file
 			currentkey = key.toString();
 			System.out.println(currentkey);
 			System.out.println("new file, new day!");
@@ -111,40 +101,44 @@ public class SonarImage {
 			bytes_length = (int) value.getLength();
 			Extract.fileProcess(key.toString());
 
-			System.out.println("not sure if this is working");
-			System.out.println("First bytes" + bytes[1]);
-
 			// output the metadata
 			StringBuilder sb = new StringBuilder();
 
 			String[][] arr = Extract.metaTemp;
-			for (int j = 0; j < arr.length; j++) {
+			for (int j = 1; j < arr.length; j++) {
+				if (arr[j][0] == null)
+					break;
 				for (int k = 0; k < arr[0].length; k++) {
-					sb.append(arr[j][k] + ",");
+					
+						sb.append(arr[j][k] + ",");
 				}
-				sb.append('\n');
+				sb.append(j + "\n");
 			}
-			mos.write("metadata", key, new Text(sb.toString()));
+			mos.write("metadata", key + "\n", new Text(sb.toString()));
 
 			// process the image bytes
-			if (all_bytes == null)
-				all_bytes = Extract.imTemp;
-			else {
-				byte[][] temp = Util.combineVertically(all_bytes, Extract.imTemp);
-//				Util.saveIm(temp, "im" + Extract.count, false);
-				image = MacroMaster.findMine(temp, "im" + Extract.count);
-				all_bytes = Extract.imTemp;
-
-				System.out.println("Finished with all");
-
-				System.out.println("img label " + Util.imgLabel);
+			if (right_bytes == null) {
+				right_bytes = Extract.imTempR;
+				left_bytes = Extract.imTempL;
+			} else {
+				// find mine
+				byte[][] temp = Util.combineVertically(right_bytes, Extract.imTempR);
+				boolean r = MacroMaster.findMine(temp, 'R');
+				right_bytes = Extract.imTempR;
+				
+				temp = Util.combineVertically(left_bytes, Extract.imTempL);
+				boolean l = MacroMaster.findMine(temp, 'L');
+				left_bytes = Extract.imTempL;
 
 				Configuration conf = new Configuration();
 				count++; // because count is updated before imgPath, count starts at 1
-				String imgPath = Util.imgLabel + count + ".png";
-
-				String localPath = basePath + Util.imgLabel + count + ".png";
-
+				System.out.println("img number " + count);
+				
+				String imgPath = "im" + count + ".png";
+				String localPath = basePath + imgPath;
+				// writes image name, associated mstiff files, and whether the image contains a mine
+				mos.write("mines", imgPath, new Text(lastkey + ", " + currentkey + ", " + (r || l)));
+				
 				// https://stackoverflow.com/questions/17488534/create-a-file-from-a-bytearrayoutputstream
 				// http://codeinventions.blogspot.com/2014/08/creating-file-from-bytearrayoutputstrea.html
 				// https://stackoverflow.com/questions/16546040/store-images-videos-into-hadoop-hdfs
@@ -154,16 +148,16 @@ public class SonarImage {
 				FileSystem fs = FileSystem.get(conf);
 				SequenceFile.Writer writer = null;
 				try {
-					// convert Util.img into a byte array
+					// convert image into a byte array
 					baos = new ByteArrayOutputStream();
-					ImageIO.write(Util.img, "png", baos);
+					ImageIO.write(Util.getImage(Util.combineHorizontally(MacroMaster.imL, MacroMaster.imR), false), "png", baos);
 					byte[] imBytes = baos.toByteArray();
 					baos.write(imBytes);
 
 					// writes image to vm folder
 					System.out.println(localPath);
 					File outfile = new File(localPath);
-					ImageIO.write(Util.img, "png", outfile);
+					ImageIO.write(Util.getImage(Util.combineHorizontally(MacroMaster.imL, MacroMaster.imR), false), "png", outfile);
 
 					// writes image as byte array as SequenceFile to hdfs
 					mos.write("image", key, new BytesWritable(imBytes));
@@ -176,8 +170,8 @@ public class SonarImage {
 
 				// wipes imTemp
 				Extract.saveMeta();
-				// context.write(new Text(key), new Text("das good"));
 			}
+			lastkey = currentkey;
 		}
 
 		public void cleanup(Context context) throws IOException, InterruptedException {
@@ -187,10 +181,11 @@ public class SonarImage {
 	}
 
 	/**
-	 * Reducer with: input key = file name (Text.class) input value = whatever the
-	 * output value of the Mapper is (class is same as Map output class) output key
-	 * = file name (Text.class) output value = Sonar image data in byte form
-	 * (BytesWritable.class)
+	 * Reducer with: input key = file name (Text.class) 
+	 * input value = whatever the output value of the Mapper is (class is same as Map output class) 
+	 * 
+	 * output key = file name (Text.class) 
+	 * output value = Sonar image data in byte form (BytesWritable.class)
 	 */
 	public static class Sonar_Reducer extends Reducer<Text, Text, Text, BytesWritable> {
 
@@ -200,9 +195,7 @@ public class SonarImage {
 	}
 
 	/**
-	 * Hadoop by default splits files into smaller chunks which are fed to the
-	 * mapper I think that this makes sure that the files aren't split when fed into
-	 * the mapper Not sure though, this was written by @author Pyojeong Kim
+	 * Keeps files from being split (?)
 	 */
 	public static class CombineDocumentFileFormat extends CombineFileInputFormat<Text, BytesWritable> {
 		@Override
@@ -237,6 +230,9 @@ public class SonarImage {
 		public void initialize(InputSplit split, TaskAttemptContext context) throws IOException, InterruptedException {
 		}
 
+		/**
+		 * reads next mstiff file
+		 */
 		@Override
 		public boolean nextKeyValue() throws IOException, InterruptedException {
 			if (!read) {
@@ -281,6 +277,7 @@ public class SonarImage {
 	public static void main(String[] args) throws Exception {
 		conf = new Configuration();
 		job = Job.getInstance(conf, "SonarImage");
+		
 		// allows for Hadoop to find the jar file
 		// by looking at each jar file and seeing if it has the following class in it
 		job.setJarByClass(SonarImage.class);
@@ -292,13 +289,16 @@ public class SonarImage {
 		// makes sure that each input to the mapper is an entire file
 		// since Hadoop typically breaks up files by default
 		job.setInputFormatClass(CombineDocumentFileFormat.class);
-		// disables reducer essentially
+		
+		// disables reducer 
 		job.setNumReduceTasks(0);
 
 		job.setOutputKeyClass(Text.class);
-		// set up 2 Hadoop jobs, one for metadata and one for sonar image
+		
+		// set up 3 outputs, one for metadata, one for sonar image, and one for mine information
 		MultipleOutputs.addNamedOutput(job, "metadata", TextOutputFormat.class, Text.class, Text.class);
 		MultipleOutputs.addNamedOutput(job, "image", SequenceFileOutputFormat.class, Text.class, BytesWritable.class);
+		MultipleOutputs.addNamedOutput(job, "mines", TextOutputFormat.class, Text.class, Text.class);
 		job.setOutputKeyClass(Text.class);
 
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
