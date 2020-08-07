265c7cf2aca591ff7b1f51bc44f2e177a8a39bf5
M. Chowdhury <mintychocolate101@gmail.com>
2020-08-07 10:24:42 -0700

    DB mods

--- modules/data_extract_db/Constants.java
+++ modules/data_extract_db/Constants.java
@@ -4,9 +4,9 @@ public class Constants {
 	/**
 	 * Change this to match your file output directory
 	 */
-	public static final String out_path = "D:\\Mitali\\ML\\NPS19\\datatest\\out";
+	public static final String out_path = "D:\\Mitali\\ML\\NPS20\\data-out";
 	/**
 	 * Change this to match your file input directory
 	 */
-	public static final String in_path =  "D:\\Mitali\\ML\\NPS19\\datatest\\in";
+	public static final String in_path =  "D:\\Mitali\\ML\\NPS19\\MOD1_Data\\131119_1";
 }
