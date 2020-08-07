0e31376485c7b2564adb7873cc771e53638d9334
Shruthik Musukula <Shruthik.musukula@gmail.com>
2020-08-06 19:35:43 -0700

    added metadata table creation scripts

---
+++ external_scripts/CreateMetadataOracleTable.sql
@@ -0,0 +1,11 @@
+CREATE TABLE shruthik.Metadata
+(
+filePath VARCHAR2(80),
+dateStamp INT,
+navTime VARCHAR2(10),
+FathTime VARCHAR2(10),
+latitude FLOAT,
+longitude FLOAT,
+waterDepth FLOAT,
+towfishDepth FLOAT
+);
